da78a80948c6326cbce2d354ad95cf5c02081e47
Karol Moroz <karol@moroz.dev>
2025-11-15 15:16:36 +0800

    Decode token

--- go.mod
+++ go.mod
@@ -11,21 +11,38 @@ require (
 	github.com/shopspring/decimal v1.4.0
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.43.0
+	golang.org/x/oauth2 v0.33.0
 	golang.org/x/text v0.30.0
+	google.golang.org/api v0.256.0
 	maragu.dev/gomponents v1.2.0
 )
 
 require (
+	cloud.google.com/go/auth v0.17.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.9.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
-	golang.org/x/oauth2 v0.33.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251103181224-f26f9409b101 // indirect
+	google.golang.org/grpc v1.76.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- handlers/oauth2_controller.go
+++ handlers/oauth2_controller.go
@@ -2,15 +2,19 @@ package handlers
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/moroz/homeosapiens-go/config"
 	"github.com/moroz/securecookie"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/idtoken"
 )
 
 type oauth2Controller struct {
@@ -48,6 +52,27 @@ func (c *oauth2Controller) GoogleRedirect(w http.ResponseWriter, r *http.Request
 	http.Redirect(w, r, url, http.StatusFound)
 }
 
+type IDTokenClaims struct {
+	Email      string `json:"email"`
+	GivenName  string `json:"given_name"`
+	FamilyName string `json:"family_name"`
+	Avatar     string `json:"picture"`
+}
+
+func decodeIDTokenClaims(token string) (*IDTokenClaims, error) {
+	segs := strings.Split(token, ".")
+	bytes, err := base64.RawURLEncoding.DecodeString(segs[1])
+	if err != nil {
+		return nil, err
+	}
+	var claims IDTokenClaims
+	if err := json.Unmarshal(bytes, &claims); err != nil {
+		return nil, err
+	}
+
+	return &claims, nil
+}
+
 func (c *oauth2Controller) GoogleCallback(w http.ResponseWriter, r *http.Request) {
 	session := r.Context().Value(config.SessionContextName).(SessionData)
 	state, _ := session[OAuth2SessionKey].(string)
@@ -67,5 +92,23 @@ func (c *oauth2Controller) GoogleCallback(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "%+v", token)
+	id_token, _ := token.Extra("id_token").(string)
+
+	validator, _ := idtoken.NewValidator(r.Context())
+	_, err = validator.Validate(r.Context(), id_token, c.config.ClientID)
+	if err != nil {
+		log.Printf("ID token verification failed: %s", err)
+		http.Error(w, fmt.Sprintf("ID token verification failed: %s", err), 500)
+		return
+	}
+
+	claims, err := decodeIDTokenClaims(id_token)
+	if err != nil {
+		log.Printf("Failed to decode ID token: %s", err)
+		http.Error(w, fmt.Sprintf("Failed to decode ID token: %s", err), 500)
+		return
+	}
+
+	pretty, _ := json.MarshalIndent(claims, "", "\t")
+	w.Write(pretty)
 }
