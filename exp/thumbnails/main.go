4c0f9ef39257473d9420cd757830285b85888936
Karol Moroz <karol@moroz.dev>
2026-04-25 17:55:11 +0200

    Programmatically render component

--- exp/thumbnails/main.go
+++ exp/thumbnails/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -10,23 +11,73 @@ import (
 	"github.com/playwright-community/playwright-go"
 )
 
+type ThumbnailProps struct {
+	PP     string `json:"pp"`
+	Title  string `json:"title"`
+	Date   string `json:"date"`
+	Host   string `json:"host"`
+	Locale string `json:"locale"` // "en" | "pl"
+}
+
+const script = `
+(async () => {
+	const mod = await import("http://localhost:5174/src/main.ts");
+	return mod.renderThumbnail;
+})()
+`
+
 func main() {
 	pw, err := playwright.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{Headless: new(false)})
+	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: new(false),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	page, err := browser.NewPage()
+	page, err := browser.NewPage(playwright.BrowserNewPageOptions{
+		Viewport: &playwright.Size{
+			Width:  640,
+			Height: 9.0 / 16 * 640,
+		},
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = page.Goto("https://moroz.dev")
+	_, err = page.Goto("http://localhost:5174", playwright.PageGotoOptions{
+		WaitUntil: playwright.WaitUntilStateDomcontentloaded,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	page.On("console", func(msg playwright.ConsoleMessage) {
+		fmt.Println("BROWSER:", msg.Text())
+	})
+
+	handle, err := page.EvaluateHandle(script)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Dispose()
+
+	props, err := json.Marshal(&ThumbnailProps{
+		PP:     "https://d3n1g0yg3ja4p3.cloudfront.net/019beef9-ad4c-736f-9bb0-965b59ca21ae.png",
+		Title:  "What prevents me from moving on?",
+		Date:   "2026-02-08",
+		Host:   "Dr Asher Shaikh",
+		Locale: "en",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = handle.Evaluate(`(fn, props) => fn(props)`, string(props))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,6 +96,10 @@ func main() {
 	}
 
 	if err := browser.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := pw.Stop(); err != nil {
 		log.Fatal(err)
 	}
 }
--- exp/thumbnails/thumbnails-svelte/src/main.ts
+++ exp/thumbnails/thumbnails-svelte/src/main.ts
@@ -1,16 +1,29 @@
-import { mount } from "svelte";
+import { mount, unmount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 
-const app = mount(App, {
-  target: document.getElementById("app")!,
-  props: {
-    pp: "https://d3n1g0yg3ja4p3.cloudfront.net/019beef9-ad4c-736f-9bb0-965b59ca21ae.png",
-    title: "What prevents me from moving on?",
-    date: "2026-02-08",
-    locale: "en",
-    host: "Dr Asher Shaikh",
-  },
-});
+export interface ThumbnailProps {
+  pp: string;
+  title: string;
+  date: string;
+  host: string;
+  locale: "en" | "pl";
+}
 
-export default app;
+let app: any = null;
+
+export function renderThumbnail(props: ThumbnailProps | string) {
+  if (typeof props === "string") {
+    props = JSON.parse(props) as ThumbnailProps;
+  }
+  app = mount(App, {
+    target: document.getElementById("app")!,
+    props,
+  });
+}
+
+export async function destroyApp() {
+  if (!app) return;
+  unmount(app);
+  app = null;
+}
