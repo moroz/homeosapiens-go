88b36c92c62536016dd53c3d70283a815b46bddd
Karol Moroz <karol@moroz.dev>
2026-05-15 22:28:18 +0200

    Rewrite locale check script in Go

--- scripts/check-locales/main.go
+++ scripts/check-locales/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"maps"
@@ -19,7 +20,10 @@ func resolveGitDir() (string, error) {
 
 	for {
 		gitDir := filepath.Join(dir, ".git")
-		stat, _ := os.Stat(gitDir)
+		stat, err := os.Stat(gitDir)
+		if !errors.Is(err, os.ErrNotExist) {
+			return "", err
+		}
 		if stat != nil && stat.IsDir() {
 			return dir, nil
 		}
@@ -44,7 +48,7 @@ func appendKey(prefix []string, key string) []string {
 	return segments
 }
 
-func mergeMap(m map[string]any, prefix []string) map[string]string {
+func flattenMap(m map[string]any, prefix []string) map[string]string {
 	result := make(map[string]string)
 	for key, value := range m {
 		switch value := value.(type) {
@@ -54,7 +58,7 @@ func mergeMap(m map[string]any, prefix []string) map[string]string {
 
 		case map[string]any:
 			newPrefix := appendKey(prefix, key)
-			merged := mergeMap(value, newPrefix)
+			merged := flattenMap(value, newPrefix)
 			maps.Copy(result, merged)
 		}
 	}
@@ -73,6 +77,48 @@ func decodeFile(path string) (map[string]any, error) {
 		return nil, err
 	}
 	return result, nil
+}
+
+var Variants = []string{"many", "other", "few", "one"}
+
+func isVariant(key string) bool {
+	segs := strings.Split(key, ".")
+	last := segs[len(segs)-1]
+	for _, variant := range Variants {
+		if last == variant {
+			return true
+		}
+	}
+	return false
+}
+
+func keyPresent(key string, otherMap map[string]string) bool {
+	if val := otherMap[key]; val != "" {
+		return true
+	}
+
+	if isVariant(key) {
+		segs := strings.Split(key, ".")
+		baseKey := strings.Join(segs[:len(segs)-1], ".")
+		if val := otherMap[baseKey]; val != "" {
+			return true
+		}
+
+		for _, variant := range Variants {
+			variantKey := fmt.Sprintf("%s.%s", baseKey, variant)
+			if val := otherMap[variantKey]; val != "" {
+				return true
+			}
+		}
+	} else {
+		for _, variant := range Variants {
+			variantKey := fmt.Sprintf("%s.%s", key, variant)
+			if val := otherMap[variantKey]; val != "" {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func main() {
@@ -98,9 +144,27 @@ func main() {
 			log.Fatal(err)
 		}
 
-		allLocales[locale] = mergeMap(data, nil)
-
-		fmt.Printf("%+v\n", allLocales[locale])
+		allLocales[locale] = flattenMap(data, nil)
 	}
 
+	valid := true
+
+	for locale, myMap := range allLocales {
+		for otherLocale, otherMap := range allLocales {
+			if locale == otherLocale {
+				continue
+			}
+
+			for key := range myMap {
+				if !keyPresent(key, otherMap) {
+					valid = false
+					fmt.Printf("Key %s is present in locale %s but not in %s\n", key, locale, otherLocale)
+				}
+			}
+		}
+	}
+
+	if !valid {
+		os.Exit(1)
+	}
 }
